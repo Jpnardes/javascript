ded1068d929ac132e5b85d3b44afc777cce348b8
João Pedro Nardes <nardes423@gmail.com>
2022-12-18 16:18:39 -0300

    js condições

---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel=12
+console.log(`A velociade do seu carro é ${vel} Km/h`)
+
+if (vel > 60) { // condição símples!//
+    console.log('Você ultrapassou a velociade permitida, MULTADO!') 
+}
+
+console.log('Dirija sempre usano cinto de seguraça!')
