ab64c7ffa6773e7cbd48c12c9d9cd7c94bc313f8
João Pedro Nardes <nardes423@gmail.com>
2023-01-10 22:22:35 -0300

    estruturas de repetição do while/ while

---
+++ ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+do{
+    
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 9) 
+
+/* Todos tem a mesma execução com diferentes métodos de repetição */
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log("Tudo Bem!")
+    c++// é o mesmo que dizer que c = c + 1
+}
+*/
+
+/*
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+*/
